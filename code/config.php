ce573be49c606702c45d055b5e552fc8c058f4df
Johan <johasel@stud.ntnu.no>
2019-01-14 14:42:29 +0100

    Create config.php

---
+++ code/config.php
@@ -0,0 +1,9 @@
+<?php
+$dbhost = "192.168.128.87";
+$dbport = "3306";
+$db = "bookface";
+$dbuser = "bookfaceuser";
+$dbpassw = 'erlend';
+$webhost = '10.212.136.137';
+$weburl = 'http://' . $webhost ;
+?>
