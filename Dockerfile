6bcce5dc4a310801cd30b39a823122a3538059f6
you@example.com <you@example.com>
2019-01-18 12:28:31 +0100

    asd

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM php:apache
+FROM php:5.6-apache
 
 RUN apt-get update
 RUN apt-get -y upgrade
 
 RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
 
-RUN php -i
 COPY code/ /var/www/html/
 
 
--- code/newuser.php
+++ code/newuser.php
@@ -8,22 +8,22 @@ echo "image:" . $image . "<br>";
 include_once "config.php";
 $link = mysqli_connect("$dbhost:$dbport", $dbuser,$dbpassw );
 	if ($link){
-#    	echo "Connection successful!\n<br>";
+    	echo "Connection successful!\n<br>";
     	$bfdb = mysqli_select_db($link,$db);
     	if ( !$bfdb ){
-#				echo "Cannot use $db: " . mysqli_error() ."<br>";
+				echo "Cannot use $db: " . mysqli_error() ."<br>";
     	} else {
-#			echo "Correct database found<br>\n";
+			echo "Correct database found<br>\n";
 			$img = mysqli_real_escape_string($link, file_get_contents($image));
-#			echo "$img</br>";
-#			$fileimg = mysqli_real_escape_string($link, file_get_contents('plot.png'));
+			echo "$img</br>";
+			$fileimg = mysqli_real_escape_string($link, file_get_contents('plot.png'));
 			$result = mysqli_query($link, "insert into user (userID,name,picture,status,posts,comments,lastPostDate,createDate) values(NULL,'$username','$img','',0,0,NULL,NULL );");
-#			echo "Result: " . mysqli_error() . "<br>\n";			
+			echo "Result: " . mysqli_error() . "<br>\n";			
 			if ( ! mysqli_error()){
 			echo "OK";
 			} else {
 				mysqli_error();
-			}	
+			}
 		}
 	}
 ?>
